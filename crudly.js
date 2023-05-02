49312c7055982c87b11d1a653337440caee27f88
DazKins <david.atkins1998@gmail.com>
2023-05-02 18:50:17 +0800

    Add custom headers and extra functionality

--- crudly.js
+++ crudly.js
@@ -12,16 +12,20 @@ const createCrudly = ({
   host = "localhost",
   port = "80",
   protocol = "https://",
+  customHeaders = {},
 }) => {
   const url = `${protocol}${host}:${port}`;
+
+  const headers = {
+    "X-PROJECT-ID": projectId,
+    "X-PROJECT-KEY": projectKey,
+    ...customHeaders,
+  }
 
   const createEntity = async (table, entity) => {
     const res = await fetch(`${url}/tables/${table}/entities`, {
       method: "POST",
-      headers: {
-        "X-PROJECT-ID": projectId,
-        "X-PROJECT-KEY": projectKey,
-      },
+      headers,
       body: JSON.stringify(entity),
     });
 
@@ -32,13 +36,22 @@ const createCrudly = ({
     return await res.text();
   };
 
+  const createEntities = async (table, entities) => {
+    const res = await fetch(`${url}/tables/${table}/entities/batch`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(entities),
+    });
+
+    if (res.status == 400) {
+      throw new CrudlyValidationError(await res.text());
+    }
+  };
+
   const putEntity = async (table, id, entity) => {
     const res = await fetch(`${url}/tables/${table}/entities/${id}`, {
       method: "PUT",
-      headers: {
-        "X-PROJECT-ID": projectId,
-        "X-PROJECT-KEY": projectKey,
-      },
+      headers,
       body: JSON.stringify(entity),
     });
 
@@ -51,10 +64,7 @@ const createCrudly = ({
 
   const getEntityById = async (table, entityId) => {
     const res = await fetch(`${url}/tables/${table}/entities/${entityId}`, {
-      headers: {
-        "X-PROJECT-ID": projectId,
-        "X-PROJECT-KEY": projectKey,
-      },
+      headers,
     });
 
     if (res.status == 404) {
@@ -70,10 +80,7 @@ const createCrudly = ({
     ).toString();
 
     const res = await fetch(`${url}/tables/${table}/entities?${queryParams}`, {
-      headers: {
-        "X-PROJECT-ID": projectId,
-        "X-PROJECT-KEY": projectKey,
-      },
+      headers,
     });
 
     return await res.json();
@@ -82,10 +89,7 @@ const createCrudly = ({
   const updateEntity = async (table, id, entity) => {
     await fetch(`${url}/tables/${table}/entities/${id}`, {
       method: "PATCH",
-      headers: {
-        "X-PROJECT-ID": projectId,
-        "X-PROJECT-KEY": projectKey,
-      },
+      headers,
       body: JSON.stringify(entity),
     });
   };
@@ -93,50 +97,61 @@ const createCrudly = ({
   const deleteEntity = async (table, id) => {
     await fetch(`${url}/tables/${table}/entities/${id}`, {
       method: "DELETE",
-      headers: {
-        "X-PROJECT-ID": projectId,
-        "X-PROJECT-KEY": projectKey,
-      },
+      headers,
     });
-  };
-
-  const getTables = async () => {
-    const res = await fetch(`${url}/tables`, {
-      method: "GET",
-      headers: {
-        "X-PROJECT-ID": projectId,
-        "X-PROJECT-KEY": projectKey,
-      },
-    });
-
-    return await res.json();
   };
 
   const createTable = async (tableName, tableSchema) => {
     const res = await fetch(`${url}/tables/${tableName}`, {
       method: "PUT",
-      headers: {
-        "X-PROJECT-ID": projectId,
-        "X-PROJECT-KEY": projectKey,
-      },
+      headers,
       body: JSON.stringify(tableSchema),
     });
 
     return await res.json();
   };
 
+  const getTableSchema = async (tableName) => {
+    const res = await fetch(`${url}/tables/${tableName}`, {
+      method: "GET",
+      headers,
+    });
+
+    return await res.json();
+  };
+
+  const getTables = async () => {
+    const res = await fetch(`${url}/tables`, {
+      method: "GET",
+      headers,
+    });
+
+    return await res.json();
+  };
+
+  const deleteTable = async (tableName) => {
+    await fetch(`${url}/tables/${tableName}`, {
+      method: "DELETE",
+      headers,
+    });
+  }
+
   return {
     createEntity,
+    createEntities,
     putEntity,
+    updateEntity,
     getEntityById,
     getEntities,
-    updateEntity,
     deleteEntity,
-    getTables,
+
     createTable,
+    getTableSchema,
+    getTables,
+    deleteTable,
   };
 };
 
 export default {
   createCrudly,
-}
+};
